bce2946d0d57d348e995e0e50dce1a7063f4878b
Adam Pelc <sikulin94@gmail.com>
2026-01-16 14:52:44 +0100

    .

--- gotils/crypto.go
+++ gotils/crypto.go
@@ -1,95 +1,86 @@
 package gotils
 
 import (
+	"fmt"
 	"maps"
-	"math"
-	"numbers"
 	"slices"
 	"unicode/utf8"
 )
 
-// const (
-// 	n0, n9   uint8 = 48, 57
-// 	aUp, zUp uint8 = 65, 90
-// 	aLo, zLo uint8 = 97, 122
-// )
-
 const runesCount = utf8.MaxRune + 1
+const (
+	n0, n9   uint8 = 48, 57
+	aUp, zUp uint8 = 65, 90
+	aLo, zLo uint8 = 97, 122
+)
 
 type runesMap map[rune]rune
 
-func GenRunesMaps(runesRangeFrom rune, runesRangeTo rune) (runesMap, runesMap, error) {
-	runesCount := runesRangeTo - runesRangeFrom + 1
-	s := make([]rune, runesCount)
-
-	for i, v := 0, runesRangeFrom; v <= runesRangeTo; v++ {
-		s[i] = v
-		i++
-	}
-
-	m := make(runesMap, runesCount)
-	m2 := make(runesMap, runesCount)
+func genRunesSets(reservedRunes []rune) (runesMap, []rune) {
+	runes := make([]rune, runesCount)
 
 	for r := range runesCount {
+		runes[r] = r
+	}
+
+	replacementsRequired := len(reservedRunes)
+
+	if replacementsRequired > runesCount {
+		panic(fmt.Errorf("Quantity of reserved runes cannot be higher than available count of runes (%w).", runesCount))
+	}
+
+	potentialReplacements := slices.Clone(runes)
+	replacements := make(runesMap, replacementsRequired)
+
+	for _, r := range reservedRunes {
 		i := 0
 
-		if l := len(s); l > 1 {
+		if l := len(potentialReplacements); l > 1 {
 			n, err := RandInt(int64(l))
 
 			if err != nil {
-				return nil, nil, err
+				panic(err)
 			}
 
 			i = int(n.Uint64())
 		}
 
-		m[r] = s[i]
-		m2[s[i]] = r  
-		s = slices.Delete(s, i, i+1)
+		replacements[r] = potentialReplacements[i]
+		potentialReplacements = slices.Delete(potentialReplacements, i, i+1)
 	}
 
-	return m, m2, nil
+	potentialNoise := Difference(runes, slices.Collect(maps.Values(replacements)))
+
+	return replacements, potentialNoise
 }
 
-func computeRunesRanges(fNoise float64) (rune, rune, rune, rune, error) {
-	qNoise := rune(Clamp(math.Round(runesCount*fNoise), 0, runesCount))
-	n, err := RandInt(2)
-
-	if err != nil {
-		return 0, 0, 0, 0, err
-	}
-
-	if n.Uint64() == 0 {
-		return 0, utf8.MaxRune - qNoise, runesCount - qNoise, utf8.MaxRune, nil
-	} else {
-		return qNoise, utf8.MaxRune, 0, qNoise - 1, nil
-	}
+type key struct {
+	replacements   runesMap
+	potentialNoise []rune
 }
 
-type key [4]runesMap
+func GenKey(reservedRunes []rune) key {
+	replacements, potentialNoise := genRunesSets(reservedRunes)
+	k := key{replacements: replacements, potentialNoise: potentialNoise}
 
-func GenKey(fNoise float64) (key, error) {
-	var k key
-
-	for i := range 4 {
-		k[i] = make(runesMap)
-	}
-
-	// var arr key = []
-
+	return k
 }
 
-func encrypt(in string, rMap runesMap, fMinNoise float32, fMaxNoise float32) (string, error) {
+func encrypt(in string, key key, fMinNoise float64, fMaxNoise float64) string {
 	out := make([]rune, len(in))
-	noiseUnavailableRunes := make([]rune, 0)
 
 	for i, r := range in {
-		out[i] = rMap[r]
-		noiseUnavailableRunes = append(noiseUnavailableRunes, rMap[r])
+		replacement, k := key.replacements[r]
+
+		if !k {
+			panic(fmt.Errorf("No replacement available for character %w. Rune key isn't present.", r))
+		}
+		
+		out[i] = replacement
 	}
 
-	noiseAvailableRunes := Difference(slices.Sorted(maps.Values(rMap)), noiseUnavailableRunes)
-	qNoiseAvailableRunes := float32(len(noiseAvailableRunes))
+	noiseAvailableRunes := Difference(slices.Sorted(maps.Values(m)), noiseUnavailableRunes)
+	qNoiseAvailableRunes := float64(len(noiseAvailableRunes))
 	minNoise := uint32(Clamp(qNoiseAvailableRunes*fMinNoise, 0, qNoiseAvailableRunes))
 	maxNoise := uint32(Clamp(qNoiseAvailableRunes*fMaxNoise, 0, qNoiseAvailableRunes))
 
